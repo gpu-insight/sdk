66ded9c42c6ade1520465e4775cb177ed0a593a1
luc <lucmann@qq.com>
2020-10-12 23:09:17 +0800

    refactor: abstract the presentation of rendering
    Signed-off-by: luc <lucmann@qq.com>

---
+++ include/iprezi.h
@@ -0,0 +1,13 @@
+//
+// Created by lulu on 2020/10/12.
+//
+// Prezi is short form of "presentation" in Hungrian.
+// iprezi abstracts a way that rendering contents are shown.
+//
+#pragma once
+
+class iprezi {
+public:
+    virtual ~iprezi() noexcept = default;
+};
+
--- src/GLUtil/GLUtilImp.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by lulu on 2020/9/14.
-//
-
-#include "GLUtilImp.h"
